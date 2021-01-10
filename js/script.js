fd06a74abc5a6622096476d9bd8bb88c56cedfa9
KROSY <45696255+krosy1337@users.noreply.github.com>
2021-01-10 15:11:34 +0300

    Added right sidebar

--- js/script.js
+++ js/script.js
@@ -15,3 +15,17 @@ testWebP(function (support) {
         document.querySelector('body').classList.add('no-webp');
     }
 });
+const menuSidebarSeeMore = document.querySelector('.menu-sidebar__item_see-more')
+const menuSidebarHiddenItems = document.querySelector('.menu-sidebar__hidden-items')
+
+menuSidebarSeeMore.addEventListener('click', event => {
+    menuSidebarHiddenItems.classList.toggle('active')
+    if (!menuSidebarHiddenItems.classList.contains('active')) {
+        menuSidebarSeeMore.querySelector('.see-more__text').textContent = 'See more...'
+        menuSidebarSeeMore.querySelector('.see-more__icon').style.transform = 'rotate(0)'
+    } else {
+        menuSidebarSeeMore.querySelector('.see-more__text').textContent = 'See less'
+        menuSidebarSeeMore.querySelector('.see-more__icon').style.transform = 'rotate(-90deg)'
+    }
+    event.preventDefault()
+})
